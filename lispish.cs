77b651a8336943e99e8478dcef6395e0471e5079
hoangnd <hoangnd@miamioh.edu>
2022-11-18 20:48:23 -0500

    Submitting

--- lispish.cs
+++ lispish.cs
@@ -1,23 +1,189 @@
 using System;
 using System.Collections.Generic;
+using  System.Text.RegularExpressions;
 
 public class LispishParser
 {
+    public  class Parser {
+        List<Node> tokens = new List<Node>();
+        int curr = 0;
+        
+        public Parser(Node [] tokens) {
+            this.tokens = new List<Node>(tokens);
+            this.tokens.Add(new Node(Symbols.INVALID, ""));
+        }
+
+        public Node ParseProgram() {
+            // Console.WriteLine($"{cur,10}: Program");
+            //<program> ::= {<expr>}
+            var children = new List<Node>();
+
+            while (tokens[curr].Symbol != Symbols.INVALID){
+                children.Add(ParseSExpr());
+                // Console.WriteLine("here");
+            }
+            
+            return new Node(Symbols.Program, children.ToArray());
+        }
+
+        public Node ParseSExpr() {
+            // decide the case 
+            if (tokens[curr].Text == "(") {
+                return new Node(Symbols.SExpr, ParseList());
+            } else {
+                return new Node(Symbols.SExpr, ParseAtom());
+            }
+        }
+
+        public Node ParseList() {
+            // first case - the list is just ()
+            if (tokens[curr].Text == "(" 
+                && (curr + 1 < tokens.Count 
+                    && tokens[curr + 1].Text == ")")) 
+            {
+                var lParen = ParseLiteral("(");
+                var rParen = ParseLiteral(")");
+                return new Node(Symbols.List, lParen, rParen);
+            }
+            // second case - the list is (<expr>)
+            else { 
+                var lParen = ParseLiteral("(");
+                var expr = ParseSeq();
+                var rParen = ParseLiteral(")");
+                return new Node(Symbols.List, lParen, expr, rParen);
+            }
+        }
+
+        public Node ParseSeq() {
+            // prepare
+            var children = new List<Node>();
+
+            // parse SExpr first
+            children.Add(ParseSExpr());
+
+            // recursively see if there's more Seq
+            if (tokens[curr].Text != ")") {
+                children.Add(ParseSeq());
+            }
+
+            // result
+            return new Node(Symbols.Seq, children.ToArray());
+        }
+
+        public Node ParseAtom() {
+            // if it's id
+            if (tokens[curr].Symbol == Symbols.ID) {
+                return new Node(Symbols.Atom, tokens[curr++]);
+            } else if (tokens[curr].Symbol == Symbols.INT) {
+                return new Node(Symbols.Atom, tokens[curr++]);
+            } else if (tokens[curr].Symbol == Symbols.REAL) {
+                return new Node(Symbols.Atom, tokens[curr++]);
+            } else if (tokens[curr].Symbol == Symbols.STRING) {
+                return new Node(Symbols.Atom, tokens[curr++]);
+            } else {
+                throw new Exception("Syntax error");
+            }
+        }
+    
+        public Node ParseLiteral(string lit) {
+            if (tokens[curr].Text == lit){
+                return tokens[curr++];
+            } else {
+                throw new Exception("Syntax error");
+            }
+        }
+    }
+
+    public enum Symbols{
+        INVALID,
+        ID, 
+        REAL,
+        INT, 
+        STRING,
+        LITERAL,
+
+        Program, 
+        SExpr, 
+        List,
+        Seq, 
+        Atom 
+    }
+
     public class Node
     {
-        public void Print(string prefix = "")
+        public Symbols Symbol;
+        public string Text = "";
+        List<Node> children = new List<Node>();
+
+        public Node(Symbols symbol, string text){
+            this.Symbol = symbol;
+            this.Text = text;
+        }
+
+        public Node(Symbols symbol, params Node[] children){
+            this.Symbol = symbol;
+            this.Text = "";
+            this.children = new List<Node>(children);
+        }
+
+        public void Print(string prefix, int padding)
         {
+            Console.WriteLine($"{prefix}{Symbol.ToString().PadRight(padding - prefix.Length)} {Text}");
+            foreach (var child in children){
+                child.Print(prefix + "  ", padding);
+            }
         }
     }
 
     static public List<Node> Tokenize(String src)
     {
-        return null;
+        // prepare
+        var result = new List<Node>();
+        Match m;
+
+        // regex parser
+        var WS = new Regex(@"\G\s");
+        var LIT = new Regex(@"\G[\\(\\)]");
+        var REAL = new Regex(@"\G(?>\+|-)?[0-9]*\.[0-9]+");
+        var INT = new Regex(@"\G(?>\+|-)?[0-9]+");
+        var STRING = new Regex(@"\G""(?>\\.|[^""])*""");
+        var ID = new Regex(@"\G[^\s""\(\)]+");
+
+        // tokenize
+        int pos = 0;
+        while (pos < src.Length) {
+            // Console.WriteLine($"pos={pos} src[pos]={src[pos]}");
+            if ((m = WS.Match(src, pos)).Success) {
+                pos += m.Length;
+            } else if ((m = LIT.Match(src, pos)).Success){
+                result.Add(new Node(Symbols.LITERAL, m.Value));
+                pos += m.Length;
+            } else if ((m = REAL.Match(src, pos)).Success){
+                result.Add(new Node(Symbols.REAL, m.Value));
+                pos += m.Length;
+            } else if ((m = INT.Match(src, pos)).Success) {
+                result.Add(new Node(Symbols.INT, m.Value));
+                pos += m.Length;
+            } else if ((m = STRING.Match(src, pos)).Success){
+                result.Add(new Node(Symbols.STRING, m.Value));
+                pos += m.Length;
+            } else if ((m = ID.Match(src, pos)).Success){
+                result.Add(new Node(Symbols.ID, m.Value));
+                pos += m.Length;
+            } else {
+                throw new Exception("Lexer error");
+            }
+        }
+
+        // return result
+        return result;
     }
 
     static public Node Parse(Node[] tokens)
     {
-        return null;
+        var parse = new Parser(tokens);
+        var program = parse.ParseProgram();
+        return program;
     }
 
     static private void CheckString(string lispcode)
@@ -35,7 +201,7 @@ public class LispishParser
             Console.WriteLine(new String('-', 50));
             foreach (Node node in tokens)
             {
-                // TODO - print the node symbol and text
+                Console.WriteLine($"{node.Symbol,-21}\t: {node.Text}");
             }
             Console.WriteLine(new String('-', 50));
 
@@ -43,7 +209,7 @@ public class LispishParser
 
             Console.WriteLine("Parse Tree");
             Console.WriteLine(new String('-', 50));
-            parseTree.Print();
+            parseTree.Print("", 40);
             Console.WriteLine(new String('-', 50));
         }
         catch (Exception)
@@ -68,6 +234,7 @@ public class LispishParser
         // CheckString(@"(+ 3.14 (* 4 7)");
 
         CheckString(Console.In.ReadToEnd());
+        // CheckString(Console.ReadLine());
     }
 }
 
